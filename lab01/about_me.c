625fd688ce73cc77f1eaf05e651fe5e8f42cb7a9
fairymavisv <z5328465@ad.unsw.edu.au>
2022-12-29 20:20:23 +1100

    First Lab

---
+++ lab01/about_me.c
@@ -0,0 +1,21 @@
+// About Me
+// about_me.c
+//
+// This program was written by YOUR-NAME-HERE (z5555555)
+// on INSERT-DATE-HERE
+//
+// A program to tell us about yourself!
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("My favourite food is: pizza\n");
+    printf("I'm doing COMP1511 because: required course\n");
+    printf("I hope to have achieved: HD by the time I finish COMP1511.\n");
+    printf("Something I'm passionate about is: C\n");
+    printf("My question about COMP1511 that hasn't been answered is: pointer\n");
+
+    return 0;
+}
+    
